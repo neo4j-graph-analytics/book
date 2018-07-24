35f5e3ffcd4298014028bd8994679c5a02f67b21
Mark Needham <m.h.needham@gmail.com>
2018-07-24 10:59:17 +0100

    update how we display shortest path results

--- scripts/path-finding/allshortestpaths/allshortestpaths.py
+++ scripts/path-finding/allshortestpaths/allshortestpaths.py
@@ -11,5 +11,18 @@ g = GraphFrame(v, e)
 
 # // tag::allshortestpaths[]
 result = g.shortestPaths(list(g.vertices.toPandas()["id"]))
-result.sort(["id", "distances"]).show(truncate=False)
-# // end::allshortestpaths[]
+result.sort(["id"]).show()
+# // end::allshortestpaths[]
+
+# // tag::allshortestpaths-more[]
+df = result.toPandas()
+df.set_index("id", inplace=True)
+
+routes = [(location, d, dest[d])
+ for location, dest in df.to_records()
+ for d in dest
+]
+
+for start, end, distance in routes[0:10]:
+    print(start, end, distance)
+# // end::allshortestpaths-more[]
