dfc7cb63d4046fdfabe3002acc7a89864c942bf0
Mark Needham <m.h.needham@gmail.com>
2018-07-24 11:18:08 +0100

    More sensible shortest path example

--- scripts/path-finding/allshortestpaths/allshortestpaths.py
+++ scripts/path-finding/allshortestpaths/allshortestpaths.py
@@ -10,19 +10,6 @@ g = GraphFrame(v, e)
 # // end::load-graph-frame[]
 
 # // tag::allshortestpaths[]
-result = g.shortestPaths(list(g.vertices.toPandas()["id"]))
-result.sort(["id"]).show()
+result = g.shortestPaths(["Colchester", "Berlin", "Hoek van Holland"])
+result.sort(["id"]).show(truncate=False)
 # // end::allshortestpaths[]
-
-# // tag::allshortestpaths-more[]
-df = result.toPandas()
-df.set_index("id", inplace=True)
-
-routes = [(location, d, dest[d])
- for location, dest in df.to_records()
- for d in dest
-]
-
-for start, end, distance in routes[0:10]:
-    print(start, end, distance)
-# // end::allshortestpaths-more[]
