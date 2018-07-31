8042dcb2821f1cda41d0c4a8465cf75ad5e50604
Mark Needham <m.h.needham@gmail.com>
2018-07-31 17:48:37 +0100

    update bfs example!

--- scripts/path-finding/bfs/bfs.py
+++ scripts/path-finding/bfs/bfs.py
@@ -1,32 +1,44 @@
-
 # // tag::imports[]
 from graphframes import *
+from pyspark.sql.types import *
 import pandas as pd
+
 # // end::imports[]
 
 # // tag::load-graph-frame[]
-v = spark.read.csv("data/transport-nodes.csv", header=True)
+fields = [
+    StructField("id", StringType(), True),
+    StructField("latitude", FloatType(), True),
+    StructField("longitude", FloatType(), True),
+    StructField("population", IntegerType(), True)
+]
+v = spark.read.csv("data/transport-nodes.csv", header=True, schema=StructType(fields))
 
 src_dst = spark.read.csv("data/transport-relationships.csv", header=True)
 df_src_dst = src_dst.toPandas()
 df_dst_src = src_dst.toPandas()
 df_dst_src.columns = ["dst", "src", "relationship", "cost"]
+
 e = spark.createDataFrame(pd.concat([df_src_dst, df_dst_src]))
 
 g = GraphFrame(v, e)
 # // end::load-graph-frame[]
 
+# // tag::candidates[]
+g.vertices.filter("population > 100000 and population < 300000").sort("population").show()
+# // end::candidates[]
+
+
 # // tag::shortestpath[]
-result = g.bfs("id='Amsterdam'", "id='London'")
+result = g.bfs("id='Den Haag'", "population > 100000 and population < 300000 and id <> 'Den Haag'")
 # // end::shortestpath[]
 
 # // tag::shortestpath-columns[]
-result.columns
+print(result.columns)
 # // end::shortestpath-columns[]
 
 
-# // tag::shortestpath-results[]
+#  // tag::shortestpath-results[]
 columns = [column for column in result.columns if not column.startswith("e")]
-
 result.select(columns).show()
-# // end::shortestpath-results[]
+#  // end::shortestpath-results[]
