f250fb8ebf54840378dcf475bec854d2fad3c620
Mark Needham <m.h.needham@gmail.com>
2018-08-06 12:18:00 +0100

    use _ not -

--- scripts/community_detection/union_find/unionfind.py
+++ scripts/community_detection/union_find/unionfind.py
@@ -1,19 +1,16 @@
 # // tag::imports[]
-from graphframes import *
-
+from pyspark.sql import functions as F
 # // end::imports[]
 
+from scripts.community_detection.import_graph import  create_software_graph
+
 # // tag::load-graph-frame[]
-v = spark.read.csv("data/sw-nodes.csv", header=True)
-e = spark.read.csv("data/sw-relationships.csv", header=True)
-g = GraphFrame(v, e)
+g = create_software_graph()
 # // end::load-graph-frame[]
 
 sc.setCheckpointDir("/tmp")
 
 # // tag::unionfind[]
-from pyspark.sql import functions as F
-
 result = g.connectedComponents()
 result.sort("component") \
     .groupby("component") \
