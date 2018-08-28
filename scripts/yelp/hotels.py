e0ef6b8630e317cdbfb459c17b679fe4188acd83
Mark Needham <m.h.needham@gmail.com>
2018-08-28 08:03:22 +0100

    pandas for this bit

--- scripts/yelp/hotels.py
+++ scripts/yelp/hotels.py
@@ -9,22 +9,24 @@ driver = GraphDatabase.driver("bolt://localhost", auth=("neo4j", "neo"))
 # end::driver[]
 
 
-# tag::vegas-hotels[]
+# tag::all-hotels[]
 query = """\
 MATCH (review:Review)-[:REVIEWS]->(business:Business),
       (business)-[:IN_CATEGORY]->(category:Category),
       (business)-[:IN_CITY]->(:City {name: $city})
-WHERE category.name in $categories
+WHERE category.name = $category
 WITH business, count(*) AS reviews, avg(review.stars) AS averageRating
 RETURN business.name AS business, reviews, averageRating
 """
 
 with driver.session() as session:
-    params = { "city": "Toronto", "categories": ["Food", "Restaurants"] }
+    params = { "city": "Las Vegas", "category": "Hotels" }
     df = pd.DataFrame([dict(record) for record in session.run(query, params)])
-# end::vegas-hotels[]
+    df = df.round(2)
+    df = df[["business", "reviews", "averageRating"]]
+# end::all-hotels[]
 
-# tag::toronto-restaurants-top-rated[]
-top_restaurants = df.sort_values(by=["reviews"], ascending=False).head(10)
-print(tabulate(top_restaurants, headers='keys', tablefmt='psql', showindex=False))
-# tag::toronto-restaurants-top-rated[]
+# tag::top-rated[]
+top_hotels = df.sort_values(by=["reviews"], ascending=False).head(10)
+print(tabulate(top_hotels, headers='keys', tablefmt='psql', showindex=False))
+# tag::top-rated[]
